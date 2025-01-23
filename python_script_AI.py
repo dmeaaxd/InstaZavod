2573ce5e9cf6180fc595e26017e3dcf290b44a03
Maksim Ushakov <dmeaaxd@gmail.com>
2025-01-23 15:03:13 +0300

    Initial commit

---
+++ python_script_AI.py
@@ -0,0 +1,470 @@
+import os
+import time
+from datetime import datetime
+
+import dotenv
+import ffmpeg
+import requests
+from aiogram import Bot
+from openai import OpenAI
+import os
+
+dotenv.load_dotenv()
+
+bot = Bot(token=os.getenv("TELEGRAM_BOT_TOKEN"))
+
+# Конфигурация API ключей и базы данных Notion
+NOTION_TOKEN = os.getenv("NOTION_TOKEN")
+NOTION_DB_ID = os.getenv("NOTION_REELS_DB_ID")
+RAPIDAPI_KEY = os.getenv("RAPID_API_KEY")
+OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+# Заголовки для запросов
+notion_headers = {
+    "Authorization": f"Bearer {NOTION_TOKEN}",
+    "Notion-Version": "2022-06-28",
+    "Content-Type": "application/json"
+}
+rapidapi_headers = {
+    "x-rapidapi-host": "social-download-all-in-one.p.rapidapi.com",
+    "x-rapidapi-key": RAPIDAPI_KEY,
+    "Content-Type": "application/json"
+}
+openai_headers = {
+    "Authorization": f"Bearer {OPENAI_API_KEY}",
+    "Content-Type": "application/json"
+}
+
+fatal_errors_count = 0
+
+
+# Получение данных из Notion
+def get_videos_from_notion():
+    url = f"https://api.notion.com/v1/databases/{NOTION_DB_ID}/query"
+    notion_headers = {
+        "Authorization": f"Bearer {NOTION_TOKEN}",
+        "Content-Type": "application/json",
+        "Notion-Version": "2022-06-28"
+    }
+
+    all_videos = []
+    has_more = True
+    next_cursor = None
+
+    # Добавляем фильтр для отбора записей с установленным чекбоксом "Одобрено"
+    filter_conditions = {
+        "property": "Одобрено",  # Имя поля чекбокса в базе данных Notion
+        "checkbox": {
+            "equals": True  # Отбираем записи, где "Одобрено" = True
+        }
+    }
+
+    while has_more:
+        payload = {
+            "filter": filter_conditions  # Добавляем фильтр в запрос
+        }
+
+        if next_cursor:
+            payload['start_cursor'] = next_cursor
+
+        response = requests.post(url, headers=notion_headers, json=payload)
+        data = response.json()
+
+        if response.status_code != 200:
+            raise Exception(f"Ошибка при получении данных из Notion: {data}")
+
+        all_videos.extend(data.get('results', []))
+
+        # Обновляем переменные для пагинации
+        has_more = data.get('has_more', False)
+        next_cursor = data.get('next_cursor')
+
+    return all_videos
+
+
+# Скачивание видео по ссылке
+def download_video(video_url):
+    global fatal_errors_count
+    url = "https://social-download-all-in-one.p.rapidapi.com/v1/social/autolink"
+    body = {"url": video_url}
+    response = requests.post(url, headers=rapidapi_headers, json=body)
+    video_data = response.json()
+    if video_data['error'] is True:
+        if ('limit' or 'token' in video_data['message']) or fatal_errors_count >= 20:
+            bot.send_message(663679771,
+                             'Лимит использования rapid api закончился или произошла критическая ошибка api\n\n' +
+                             video_data['message'])
+            return None
+    video_response = requests.get(video_data['medias'][0]['url'])
+    with open("downloaded_video.mp4", 'wb') as f:
+        f.write(video_response.content)
+    return video_data['medias'][0]['url']
+
+
+# Преобразование видео в аудио
+def convert_video_to_audio(video_file):
+    audio_file = video_file.replace(".mp4", ".mp3")
+    ffmpeg.input(video_file).output(audio_file).run()
+    return audio_file
+
+
+# Транскрибация с Whisper
+def transcribe_audio(audio_file):
+    audio_file = open("downloaded_video.mp3", "rb")
+    transcription = client.audio.transcriptions.create(
+        model="whisper-1",
+        file=audio_file,
+        response_format="text"
+    )
+
+    audio_file.close()
+
+    print(transcription)
+
+    return transcription
+
+
+# Определение языка с помощью GPT
+def detect_language(text):
+    url = "https://api.openai.com/v1/chat/completions"
+    data = {
+        "model": "gpt-3.5-turbo",
+        "messages": [
+            {
+                "role": "user",
+                "content": f"Определи язык этого текста, и верни ответ в виде короткого буквенного кода (ru, en, es): {text}"
+            }
+        ]
+    }
+    response = requests.post(url, headers=openai_headers, json=data)
+    language_data = response.json()
+    return language_data["choices"][0]["message"]["content"].strip()
+
+
+# Перевод текста с помощью OpenAI
+def translate_text_with_openai(text):
+    url = "https://api.openai.com/v1/chat/completions"
+    data = {
+        "model": "gpt-3.5-turbo",
+        "messages": [
+            {
+                "role": "user",
+                "content": f"Переведи следующий текст на русский: {text}"
+            }
+        ]
+    }
+    response = requests.post(url, headers=openai_headers, json=data)
+    language_data = response.json()
+    return language_data["choices"][0]["message"]["content"].strip()
+
+
+# Добавление транскрибации и уникализированного текста в Notion
+def update_notion_properties(page_id, stage, status):
+    url = f"https://api.notion.com/v1/pages/{page_id}"
+
+    current_date = datetime.now().strftime("%Y-%m-%d")
+
+    if status is None:
+        data = {
+            "properties": {
+                "Этап": {
+                    "select": {
+                        "name": stage
+                    }
+                }
+            }
+        }
+    else:
+        data = {
+            "properties": {
+                "Этап": {
+                    "select": {
+                        "name": stage
+                    }
+                },
+                "Статус": {
+                    "status": {
+                        "name": status
+                    }
+                }
+            }
+        }
+
+    response = requests.patch(url, headers=notion_headers, json=data)
+
+    if response.status_code == 200:
+        print("Successfully updated Notion page properties.")
+    else:
+        raise Exception(f"Error updating Notion page properties: {response.json()}")
+
+
+def add_notion_blocks(page_id, unique_text, headers_text, transcribe):
+    url = f"https://api.notion.com/v1/blocks/{page_id}/children"
+
+    data = {
+        "children": [
+            {
+                "object": "block",
+                "type": "heading_3",
+                "heading_3": {
+                    "rich_text": [
+                        {
+                            "type": "text",
+                            "text": {
+                                "content": "Транскрибация:"
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                "object": "block",
+                "type": "paragraph",
+                "paragraph": {
+                    "rich_text": [
+                        {
+                            "type": "text",
+                            "text": {
+                                "content": transcribe
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                "object": "block",
+                "type": "heading_3",
+                "heading_3": {
+                    "rich_text": [
+                        {
+                            "type": "text",
+                            "text": {
+                                "content": "Новый сценарий:"
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                "object": "block",
+                "type": "paragraph",
+                "paragraph": {
+                    "rich_text": [
+                        {
+                            "type": "text",
+                            "text": {
+                                "content": unique_text
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                "object": "block",
+                "type": "heading_3",
+                "heading_3": {
+                    "rich_text": [
+                        {
+                            "type": "text",
+                            "text": {
+                                "content": "5 заголовков:"
+                            }
+                        }
+                    ]
+                }
+            },
+            {
+                "object": "block",
+                "type": "paragraph",
+                "paragraph": {
+                    "rich_text": [
+                        {
+                            "type": "text",
+                            "text": {
+                                "content": headers_text
+                            }
+                        }
+                    ]
+                }
+            }
+        ]
+    }
+
+    response = requests.patch(url, headers=notion_headers, json=data)
+
+    if response.status_code == 200:
+        print("Successfully added blocks to Notion page.")
+    else:
+        raise Exception(f"Error adding blocks to Notion page: {response.json()}")
+
+
+def cant_transcribe(page_id):
+    url = f"https://api.notion.com/v1/blocks/{page_id}/children"
+
+    data = {
+        "children": [
+            {
+                "object": "block",
+                "type": "paragraph",
+                "paragraph": {
+                    "rich_text": [
+                        {
+                            "type": "text",
+                            "text": {
+                                "content": "Не удалось сделать транскрибацию. Текст отсутствует или слишком короткий."
+                            }
+                        }
+                    ]
+                }
+            }
+        ]
+    }
+
+    response = requests.patch(url, headers=notion_headers, json=data)
+
+
+# Функция ожидания завершения работы ассистента
+def wait_on_run(run, thread):
+    while run.status == "queued" or run.status == "in_progress":
+        run = client.beta.threads.runs.retrieve(
+            thread_id=thread.id,
+            run_id=run.id,
+        )
+        time.sleep(0.5)
+    return run
+
+
+def submit_message(assistant_id, thread, user_message):
+    client.beta.threads.messages.create(
+        thread_id=thread.id, role="user", content=user_message
+    )
+    return client.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=assistant_id,
+    )
+
+
+def get_response(thread):
+    # Получаем объект SyncCursorPage, содержащий список сообщений
+    response = client.beta.threads.messages.list(thread_id=thread.id, order="asc")
+
+    # Доступ к данным сообщений через .data
+    return response.data
+
+
+def create_thread_and_run(user_input, assistant_id):
+    thread = client.beta.threads.create()
+    run = submit_message(assistant_id, thread, user_input)
+    return thread, run
+
+
+def pretty_print(messages):
+    print("# Messages")
+    for m in messages:
+        print(f"{m.role}: {m.content[0].text.value}")
+
+
+def get_unique_text_from_assistant(transcription_text):
+    # Создаем новый поток общения с ассистентом и отправляем запрос на уникализацию текста
+    thread, run = create_thread_and_run(f"{transcription_text}", os.getenv("TRANSCRIP_ASSISTANT"))
+    run = wait_on_run(run, thread)
+
+    # Получаем ответ
+    messages = get_response(thread)
+    response_message = messages[-1].content[0].text.value
+    return response_message
+
+
+def get_headers_from_assistant(unique_text):
+    # Создаем новый поток общения с ассистентом и отправляем запрос на создание заголовков
+    thread, run = create_thread_and_run(f"{unique_text}", os.getenv("HEADERS_ASSISTANT"))
+    run = wait_on_run(run, thread)
+
+    # Получаем ответ
+    messages = get_response(thread)
+    response_message = messages[-1].content[0].text.value
+    return response_message
+
+
+# Основной процесс обработки
+def process_videos():
+    global fatal_errors_count
+    while True:
+        videos = get_videos_from_notion()
+        for video in videos:
+            try:
+                approved = video["properties"]["Одобрено"]["checkbox"]
+                status = video["properties"]["Статус"]['status']['name']
+                stage = video["properties"]["Этап"]['select']
+                if approved and status == 'N/A' and (stage is None or stage['name'] == 'AI'):
+                    print(video)
+                    page_id = video["id"]
+                    video_url = video["properties"]["Референс"]["url"]
+
+                    # Обновляем статус на AI
+                    update_notion_properties(page_id, "AI", None)
+
+                    # Скачиваем видео
+
+                    tries = 0
+
+                    while True:
+                        try:
+                            print(1)
+                            video_file_url = download_video(video_url)
+                            fatal_errors_count = 0
+                            break
+                        except Exception as e:
+                            print(e)
+                            fatal_errors_count += 1
+                            tries += 1
+                            if tries > 3:
+                                break
+                    if tries > 3:
+                        print(f"Ошибка при скачивании видео: {video_url}")
+                        continue
+
+                    # Преобразуем видео в аудио
+                    audio_file = convert_video_to_audio(os.getcwd() + "/downloaded_video.mp4")
+
+                    # Транскрибируем аудио
+                    transcript_orig = transcribe_audio(audio_file)
+                    if not transcript_orig or len(transcript_orig.split()) < 5:
+                        os.remove('downloaded_video.mp4')
+                        os.remove('downloaded_video.mp3')
+                        print('Не удалось транскрибировать')
+                        cant_transcribe(page_id)
+                        update_notion_properties(page_id, "СЦЕНАРИЙ", "ВЗЯТЬ В РАБОТУ")
+                        continue
+
+                    os.remove('downloaded_video.mp4')
+                    os.remove('downloaded_video.mp3')
+
+                    # Определяем язык
+                    language = detect_language(transcript_orig)
+
+                    if language != "ru":
+                        transcript = translate_text_with_openai(transcript_orig)
+                    else:
+                        transcript = transcript_orig
+                    # Уникализируем текст
+                    unique_text = get_unique_text_from_assistant(transcript)
+
+                    # Генерируем заголовки
+                    headers_text = get_headers_from_assistant(unique_text)
+
+                    # Обновляем свойства страницы в Notion
+                    update_notion_properties(page_id, "СЦЕНАРИЙ", "ВЗЯТЬ В РАБОТУ")
+
+                    # Добавляем блоки с текстом в Notion
+                    add_notion_blocks(page_id, unique_text, headers_text, transcript_orig)
+            except Exception as e:
+                print(e)
+
+        time.sleep(60)
+
+
+if __name__ == "__main__":
+    process_videos()
---
+++ python_script_parser.py
@@ -0,0 +1,309 @@
+from datetime import datetime, timedelta
+
+import dotenv
+import pytz
+import requests
+import os
+
+dotenv.load_dotenv()
+
+# Конфигурационные параметры
+NOTION_TOKEN = os.getenv("NOTION_TOKEN")
+NOTION_DONORS_DB_ID = os.getenv("NOTION_DONORS_DB_ID")
+NOTION_REELS_DB_ID = os.getenv("NOTION_REELS_DB_ID")
+RAPIDAPI_KEY = os.getenv("RAPID_API_KEY")
+DAYS_TO_FETCH = 90  # Количество дней для сбора Reels
+MIN_DAYS_OLD = 5  # Минимальный возраст Reels для расчета среднего числа просмотров
+
+# Заголовки для запросов к Notion API
+notion_headers = {
+    "Authorization": f"Bearer {NOTION_TOKEN}",
+    "Notion-Version": "2022-06-28",
+    "Content-Type": "application/json"
+}
+
+
+# Функция для получения списка доноров из Notion
+def get_donors_from_notion():
+    url = f"https://api.notion.com/v1/databases/{NOTION_DONORS_DB_ID}/query"
+    response = requests.post(url, headers=notion_headers)
+    data = response.json()
+    donors = []
+    for result in data['results']:
+        try:
+            properties = result['properties']
+            username = properties['username']['title'][0]['text']['content']
+            donor_id = result['id']
+            donors.append({'username': username, 'donor_id': donor_id})
+        except:
+            pass
+    return donors
+
+
+# Функция для получения Reels от доноров
+def get_reels_from_donor(username):
+    url = "https://instagram-scraper-api2.p.rapidapi.com/v1.2/reels"
+    headers = {
+        "X-RapidAPI-Key": RAPIDAPI_KEY,
+        "X-RapidAPI-Host": "instagram-scraper-api2.p.rapidapi.com"
+    }
+    reels = []
+    pagination_token = None
+    threshold_date = datetime.now() - timedelta(days=DAYS_TO_FETCH)
+    while True:
+        querystring = {
+            "username_or_id_or_url": username,
+            "pagination_token": pagination_token
+        }
+        response = requests.get(url, headers=headers, params=querystring)
+        if response.status_code == 200:
+            data = response.json()
+            items = data.get("data", {}).get("items", [])
+            for item in items:
+                try:
+                    created_at = datetime.fromtimestamp(item.get('caption', {}).get('created_at'))
+                    if created_at < threshold_date:
+                        return reels
+                    reels.append(item)
+                except Exception as e:
+                    pass
+            pagination_token = data.get("pagination_token")
+            if not pagination_token:
+                break
+        else:
+            print(f"Ошибка при получении Reels для {username}: {response.text}")
+            break
+    return reels
+
+
+# Функция для добавления или обновления Reels в Notion
+def upsert_reel_in_notion(reel_data, average_views):
+    # Проверяем, существует ли Reel в базе Notion
+    reel_id = reel_data['id']
+    search_url = f"https://api.notion.com/v1/databases/{NOTION_REELS_DB_ID}/query"
+    filter_data = {
+        "filter": {
+            "property": "ID",
+            "number": {
+                "equals": int(reel_id)
+            }
+        }
+    }
+    response = requests.post(search_url, headers=notion_headers, json=filter_data)
+    print(response.json())
+    results = response.json().get('results', [])
+    if results:
+        print(0)
+        # Обновляем существующий Reel
+        page_id = results[0]['id']
+        update_url = f"https://api.notion.com/v1/pages/{page_id}"
+        properties = construct_reel_properties(reel_data, average_views)
+        data = {"properties": properties}
+        response = requests.patch(update_url, headers=notion_headers, json=data)
+    else:
+        print(1)
+        # Добавляем новый Reel
+        create_url = "https://api.notion.com/v1/pages"
+        properties = construct_reel_properties(reel_data, average_views)
+        data = {
+            "parent": {"database_id": NOTION_REELS_DB_ID},
+            "properties": properties
+        }
+        response = requests.post(create_url, headers=notion_headers, json=data)
+    if response.status_code in [200, 201]:
+        print(f"Reel {reel_id} успешно обновлен/добавлен в Notion.")
+    else:
+        print(f"Ошибка при обновлении/добавлении Reel {reel_id}: {response.text}")
+
+
+# Функция для построения свойств Reel для Notion
+def construct_reel_properties(reel_data, average_views):
+    reel_id = reel_data['id']
+    created_at = datetime.fromtimestamp(reel_data.get('caption', {}).get('created_at'))
+    code = reel_data.get('code', '')
+    link = f"https://www.instagram.com/reel/{code}"
+    username = reel_data.get('user', {}).get('username', '')
+    caption_text = reel_data.get('caption', {}).get('text', '')
+    if caption_text:
+        caption_words = caption_text.split()
+        if len(caption_words) > 7:
+            title = ' '.join(caption_words[:7])
+        else:
+            title = caption_text
+    else:
+        title = reel_id
+    play_count = reel_data.get('play_count', 0) or 0
+    like_count = reel_data.get('like_count', 0) or 0
+    comment_count = reel_data.get('comment_count', 0) or 0
+    reshare_count = reel_data.get('reshare_count', 0) or 0
+    ER = round((reshare_count / play_count) * 100, 2) if play_count > 0 else 0
+    KF = round(play_count / average_views, 2) if average_views > 0 else 0
+    properties = {
+        "Дата референса": {"date": {"start": created_at.isoformat()}},
+        "Референс": {"url": link},
+        "Автор": {"rich_text": [{"text": {"content": username}}]},
+        "Название": {"title": [{"text": {"content": title}}]},
+        "Просмотры": {"number": play_count},
+        "Лайки": {"number": like_count},
+        "Комменты": {"number": comment_count},
+        "Репосты": {"number": reshare_count},
+        "ER": {"number": ER},
+        "КФ": {"number": KF},
+        "ID": {"number": int(reel_id)},
+    }
+    return properties
+
+
+# Функция для обновления информации о донорах
+def update_donor_info(donor, average_views):
+    username = donor['username']
+    donor_id = donor['donor_id']
+
+    # Получение данных из Instagram
+    url = f"https://instagram-scraper-api2.p.rapidapi.com/v1/info?username_or_id_or_url={username}"
+    headers_instagram = {
+        "X-RapidAPI-Key": RAPIDAPI_KEY,
+        "X-RapidAPI-Host": "instagram-scraper-api2.p.rapidapi.com"
+    }
+    response = requests.get(url, headers=headers_instagram)
+    if response.status_code == 200:
+        data = response.json()
+        user_data = data.get('data', {})
+        follower_count = user_data.get('follower_count', 0)
+        previous_followers = donor.get('followers', 0)
+        growth = follower_count - previous_followers if previous_followers else 0
+
+        # Обновление информации о доноре в Notion
+        update_url = f"https://api.notion.com/v1/pages/{donor_id}"
+        properties = {
+            "Ссылка": {"url": "https://www.instagram.com/" + username},
+            "Подписчики": {"number": follower_count},
+            "Прирост за неделю": {"number": growth},
+            "Среднее число просмотров": {"number": average_views},
+            "ID": {"rich_text": [{"text": {"content": donor_id}}]}
+        }
+        response = requests.patch(update_url, headers=notion_headers, json={"properties": properties})
+        if response.status_code == 200:
+            print(f"Информация о доноре {username} успешно обновлена.")
+        else:
+            print(f"Ошибка при обновлении донора {username}: {response.text}")
+    else:
+        print(f"Ошибка при получении данных о доноре {username}: {response.text}")
+
+
+def get_videos_from_notion():
+    url = f"https://api.notion.com/v1/databases/{NOTION_REELS_DB_ID}/query"
+    all_videos = []
+    has_more = True
+    next_cursor = None
+    while has_more:
+        payload = {}
+        if next_cursor:
+            payload['start_cursor'] = next_cursor
+
+        response = requests.post(url, headers=notion_headers, json=payload)
+        data = response.json()
+
+        if response.status_code != 200:
+            raise Exception(f"Ошибка при получении данных из Notion: {data}")
+
+        all_videos.extend(data.get('results', []))
+
+        # Обновляем переменные для пагинации        has_more = data.get('has_more', False)
+        next_cursor = data.get('next_cursor')
+
+    return all_videos
+
+
+def clean_old_reels():
+    reels = get_videos_from_notion()
+    threshold_date = datetime.now() - timedelta(days=90)
+    for reel in reels:
+        try:
+            properties = reel['properties']
+            status = properties.get('Статус', {}).get('status', {}).get('name', '')
+            if properties.get('Этап', {}).get('select', {}) is not None:
+                stage = properties.get('Этап', {}).get('select', {}).get('name', '')
+            else:
+                stage = None
+                created_at_str = properties.get('Дата референса', {}).get('date', {}).get('start', '')
+            if created_at_str:
+                utc = pytz.UTC
+                created_at = datetime.fromisoformat(created_at_str)
+
+                if created_at < utc.localize(threshold_date):
+                    if created_at < utc.localize(threshold_date) and (status == 'N/A' and stage is None):
+                        # Удаляем Reel                        page_id = reel['id']
+                        delete_url = f"https://api.notion.com/v1/pages/{page_id}"
+                        response = requests.patch(delete_url, headers=notion_headers)
+                        if response.status_code == 200:
+                            print(f"Reel {page_id} успешно удален.")
+                        else:
+                            print(f"Ошибка при удалении Reel {page_id}: {response.text}")
+        except Exception as e:
+            print(e)
+
+
+# Основная функция
+def main():
+    donors = get_donors_from_notion()
+    for donor in donors:
+        try:
+            username = donor['username']
+
+            reels = get_reels_from_donor(username)
+
+            current_date = datetime.now()
+            reels_for_average = [reel for reel in reels if (current_date - datetime.fromtimestamp(
+                reel.get('caption', {}).get('created_at'))).days >= MIN_DAYS_OLD]
+            if reels_for_average:
+                total_views = sum(reel.get('play_count', 0) or 0 for reel in reels_for_average)
+                average_views = total_views / len(reels_for_average)
+            else:
+                average_views = 0
+            for reel in reels:
+                upsert_reel_in_notion(reel, average_views)
+
+            update_donor_info(donor, round(average_views))
+        except:
+            pass
+
+    clean_old_reels()
+
+
+def archive_all_videos_from_notion():
+    while True:
+        # URL для запроса всех записей из базы данных
+        url = f"https://api.notion.com/v1/databases/{NOTION_REELS_DB_ID}/query"
+
+        # Получаем все записи из таблицы
+        response = requests.post(url, headers=notion_headers)
+        data = response.json()
+        results = data.get('results', [])
+
+        if not results:
+            print("В базе данных нет записей для архивации.")
+            break
+            return
+
+        # Архивируем каждую запись
+        for result in results:
+            page_id = result['id']
+            archive_url = f"https://api.notion.com/v1/pages/{page_id}"
+            data = {
+                "archived": True  # Архивируем запись
+            }
+            response = requests.patch(archive_url, headers=notion_headers, json=data)
+            if response.status_code == 200:
+                print(f"Запись {page_id} успешно заархивирована.")
+            else:
+                break
+                print(f"Ошибка при архивации записи {page_id}: {response.text}")
+
+
+# Пример использования функции
+# archive_all_videos_from_notion()
+
+
+if __name__ == "__main__":
+    main()
